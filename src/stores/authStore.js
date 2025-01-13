33c9fb443278009a3f7531773e81b4490b0aab46
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-13 16:05:22 -0300

    Agregar manejo del usuario autenticado en authStore y mejorar el registro de credenciales

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -10,12 +10,15 @@ export const useAuthStore = defineStore('auth', () => {
     'auth/invalid-credential': 'El usuario o la contraseña son incorrectos',
   };
 
+  const authUser = ref({});
   // firebase
   const auth = useFirebaseAuth();
   const login = ({ email, password }) => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredentials) => {
-        console.log(userCredentials)
+        const user = userCredentials.user;
+        authUser.value = user;
+        console.log(authUser.value);
       })
       .catch((error) => {
         // Obtener el mensaje de error traducido o usar un mensaje genérico
