ea9d298901d44e18090b83f58150781d28dd092b
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-21 17:42:49 -0300

    Agregar protección de rutas en el enrutador, requiriendo autenticación para acceder al panel de administración

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { onAuthStateChanged } from 'firebase/auth';
+import { useFirebaseAuth } from 'vuefire';
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -12,12 +14,13 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: () => import('@/views/LoginView.vue')
+      component: () => import('@/views/LoginView.vue'),
     },
     {
       path: '/admin',
       name: 'admin',
       component: () => import('@/views/admin/AdminLayout.vue'),
+      meta: { requiresAuth: true },
       children: [
         {
           path: '/admin/propiedades',
@@ -37,5 +40,36 @@ const router = createRouter({
     }
   ],
 })
+
+// Guard
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some(url => url.meta.requiresAuth);
+  if (requiresAuth) {
+    try {
+      await authenticateUser();
+      next();
+    } catch (error) {
+      console.error(error);
+      next({ name: 'login' });
+    }
+  } else {
+    next();
+  }
+});
+
+function authenticateUser() {
+  const auth = useFirebaseAuth();
+
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      unsubscribe()
+      if (user) {
+        resolve(user);
+      } else {
+        reject();
+      }
+    })
+  });
+}
 
 export default router
