4463abc1844ca8cfe81b22b5ba6cd56f7f1af0df
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-21 22:29:47 -0300

    Agregar funcionalidad para seleccionar y previsualizar imágenes en la vista de nueva propiedad, incluyendo manejo de ubicación en el mapa

--- src/composables/useLocationMap.js
+++ src/composables/useLocationMap.js
@@ -5,8 +5,15 @@ export default function useLocationMap() {
   const zoom = ref(12);
   const center = ref([-33.4514073, -70.6457466]);
 
+  function pin(e) {
+    const marker = e.target.getLatLng();
+
+    center.value = [marker.lat, marker.lng];
+  }
+
   return {
     zoom,
-    center
+    center,
+    pin
   }
 }
--- src/views/admin/NuevaPropiedadView.vue
+++ src/views/admin/NuevaPropiedadView.vue
@@ -1,21 +1,32 @@
 <script setup>
 import { useForm, useField } from 'vee-validate';
-import { validationSchema, imageSchema } from '@/schemas/nuevapropiedadSchema'
 import { collection, addDoc } from 'firebase/firestore';
 import { useFirestore } from 'vuefire';
 import { useRouter } from 'vue-router';
-import useImage from '@/composables/useImage';
+import { validationSchema, imageSchema } from '@/schemas/nuevapropiedadSchema'
+// import useImage from '@/composables/useImage';
 import useLocationMap from '@/composables/useLocationMap';
 import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import { ref } from 'vue';
 
-const items = [0, 1, 2, 3, 4, 5];
-const { uploadImage, image, url } = useImage();
-const { zoom, center } = useLocationMap();
+const items = [1, 2, 3, 4, 5];
+
+// const { url, uploadImage, image } = useImage();
+const { zoom, center, pin } = useLocationMap();
+
 const router = useRouter();
 const db = useFirestore();
+
+const { handleSubmit } = useForm({
+  validationSchema: {
+    ...validationSchema,
+    ...imageSchema
+  }
+});
+
 const titulo = useField('titulo');
-const imagen = useField('imagen');
+// const imagen = useField('imagen');
 const precio = useField('precio');
 const habitaciones = useField('habitaciones');
 const wc = useField('wc');
@@ -26,26 +37,33 @@ const alberca = useField('alberca', null, {
   initialValue: false
 });
 
-const { handleSubmit } = useForm({
-  validationSchema: {
-    ...validationSchema,
-    ...imageSchema
-  }
-});
+const imageOptions = ref([
+  'Aqcua mare',
+  'Aqcua mare luxury',
+  'Terranova',
+  'Terranova luxury',
+  'Zen noir',
+  'Zen lumiere'
+]);
+
+const selectedImage = ref(null);
+
+const getImageUrl = (imageName) => {
+  return `/${imageName}.jpg`;
+};
 
 const submit = handleSubmit(async (values) => {
-
   const { ...propiedad } = values;
-
   const docRef = await addDoc(collection(db, "propiedades"), {
     ...propiedad,
-    imagen: url.value
+    // imagen: url.value,
+    imagen: selectedImage.value,
+    ubicacion: center.value
   });
 
   if (docRef.id) {
     router.push({ name: 'admin-propiedades' });
   }
-
 });
 
 </script>
@@ -64,12 +82,19 @@ const submit = handleSubmit(async (values) => {
       <v-text-field class="mb-5" label="Titulo Propiedad" v-model="titulo.value.value"
         :error-messages="titulo.errorMessage.value" />
 
-      <v-file-input accept="image/jpeg" label="Fotografía" prepend-icon="mdi-camera" class="mb-5"
+      <!-- <v-file-input accept="image/jpeg" label="Fotografía" prepend-icon="mdi-camera" class="mb-5"
         v-model="imagen.value.value" :error-messages="imagen.errorMessage.value" @change="uploadImage" />
 
       <div v-if="image" class="my-5">
         <p class="font-weight-bold">Imagen Propiedad: </p>
         <img :src="image" class="w-50">
+      </div> -->
+
+      <v-select label="Selecciona una imagen" :items="imageOptions" class="mb-5" v-model="selectedImage" />
+
+      <div v-if="selectedImage" class="my-5">
+        <p class="font-weight-bold">Vista previa de la imagen:</p>
+        <img :src="getImageUrl(selectedImage)" class="w-50" />
       </div>
 
       <v-text-field class="mb-5" label="Precio" v-model="precio.value.value"
@@ -99,9 +124,9 @@ const submit = handleSubmit(async (values) => {
 
       <h2 class="font-weigth-bold text-center my-5">Ubicación</h2>
       <div class="pb-5">
-        <div style="height:600px">
+        <div style="height:400px">
           <LMap v-model:zoom="zoom" :center="center" :use-global-leaflet="false">
-            <LMarker :lat-lng="center" draggable="true" />
+            <LMarker :lat-lng="center" draggable="true" @moveend="pin" />
             <LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png">
             </LTileLayer>
           </LMap>
