3775ea9a70d99780a68328d08da486727af2dd2d
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-23 19:34:46 -0300

    Agregar funcionalidad para listar propiedades en la vista de administración, incluyendo integración con Firestore

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -1,5 +1,8 @@
 <script setup>
+import usePropiedades from '@/composables/usePropiedades';
 
+const { propiedadesCollection } = usePropiedades();
+console.log(propiedadesCollection);
 </script>
 
 <template>
@@ -8,6 +11,41 @@
   <v-btn color="blue" variant="flat" :to="{ name: 'nueva-propiedad' }">
     Nueva Propiedad
   </v-btn>
+
+  <v-card class="mx-auto mt-10 pa-5">
+    <v-list>
+      <v-list-item v-for="propiedad in propiedadesCollection" :key="propiedad.id">
+        <!-- imagen -->
+        <template v-slot:prepend>
+          <v-list-item-media start>
+            <img :src="`/${propiedad.imagen}.jpg`" alt="`Imagen de la propiedad ${propiedad.titulo}`" width="250">
+          </v-list-item-media>
+        </template>
+
+        <!-- titulo -->
+        <v-list-item-title>
+          {{ propiedad.titulo }}
+        </v-list-item-title>
+
+        <!-- precio -->
+        <v-list-item-subtitle>
+          {{ propiedad.precio }}
+        </v-list-item-subtitle>
+
+        <!-- acciones -->
+        <template v-slot:append>
+          <v-btn color="warning" flat class="mr-2">
+            Editar
+          </v-btn>
+
+          <v-btn color="error" flat>
+            Eliminar
+          </v-btn>
+        </template>
+
+      </v-list-item>
+    </v-list>
+  </v-card>
 </template>
 
 <style scoped></style>
