2411ff84b7dabd5ffe3cbc102c1f1f3eed9bc408
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-24 16:45:56 -0300

    Agregar vista de propiedades en HomeView, incluyendo componente PropiedadComp para mostrar detalles y formateo de precios

---
+++ src/components/PropiedadComp.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  propiedad: {
+    type: Object,
+  },
+  formatearPrecio: {
+    type: Function,
+  }
+})
+</script>
+
+<template>
+  <v-col cols="12" md="4">
+    <v-card>
+      <v-img :src="`/${propiedad.imagen}.jpg`" height="230" cover />
+      <v-card-title>
+        {{ propiedad.titulo }}
+      </v-card-title>
+
+      <v-card-text class="text-truncate">
+        {{ propiedad.descripcion }}
+      </v-card-text>
+
+      <v-card-text class="text-h6 font-weight-bold">
+        Precio: {{ formatearPrecio(propiedad.precio) }}
+      </v-card-text>
+
+      <template v-slot:actions>
+        <v-btn block color="info" variant="tonal">Ver más</v-btn>
+      </template>
+    </v-card>
+  </v-col>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,17 @@
 <script setup>
+import usePropiedades from '@/composables/usePropiedades';
+import PropiedadComp from '@/components/PropiedadComp.vue';
 
+const { propiedadesCollection, formatearPrecio } = usePropiedades();
 </script>
 
 <template>
-  <h1 class="text-center text-h3 font-weight-bold my-5">Vue Bienes Raices</h1>
+  <h1 class="text-center text-h3 font-weight-bold my-5">Nuestras Propiedades</h1>
+
+  <v-card flat class="py-10">
+    <v-row>
+      <PropiedadComp v-for="propiedad in propiedadesCollection" :key="propiedad.id" :propiedad="propiedad"
+        :formatearPrecio="formatearPrecio" />
+    </v-row>
+  </v-card>
 </template>
